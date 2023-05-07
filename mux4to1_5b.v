15c481c9b6c648af36352d6dadb4f1c4c3e87ddb
zahrahojati19 <129644190+zahrahojati19@users.noreply.github.com>
2023-05-07 20:24:56 +0330

    Add files via upload

---
+++ mux4to1_5b.v
@@ -0,0 +1,11 @@
+module mux4to1_5b (i0, i1, i2, i3, sel, y);
+  input [4:0] i0, i1, i2, i3;
+  input [1:0]sel;
+  output [4:0] y;
+  
+  assign y = (sel==2'b00) ? i0 :
+	     (sel==2'b01) ? i1 :
+	     (sel==2'b10) ? i2 :
+	     (sel==2'b11) ? i3 : 5'bx;
+  
+endmodule
